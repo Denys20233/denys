b8713f64e27843ef679ea7d26c430554d9ef8dd1
Denys20233 <146827659+Denys20233@users.noreply.github.com>
2023-10-07 10:09:37 +0000

    ehfreywr

--- Hello.py
+++ Hello.py
@@ -677,10 +677,10 @@ def visualization(selected_project):
         st.image("images/graph11.png", output_format="auto")
         # st.image("images/google sheets 6 (1) (1).jpg", use_column_width=True)
 
-        screenshot_path = "images/google sheets 6 (1) (1).jpg"
+        screenshot_path = "/workspaces/denys/images/google sheets 6 (1) (1).jpg"
 
         try:
-            st.write("2")
+            st.write("3")
             st.image(screenshot_path, use_column_width=True)
         except Exception as e:
             # Виведення повідомлення про помилку, якщо не вдалося відобразити зображення
